73c93a46f2c2c3c60379ffc90968acb9e8595150
Markus Skogsmo <markus.skogsmo.93@gmail.com>
2022-02-06 17:10:59 +0100

    added images and instructions of how to create api

---
+++ keys_variables_example.py
@@ -0,0 +1,14 @@
+google_vision_api_key_dictionary ={
+  "type": "service_account",
+  "project_id": "sacred-archway-338609",
+  "private_key_id": "12877fcd09ca8702296b546b275520c747a35e76",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDCNFhzRXNOoTRf\nl0Pzw8p7Ojp0+x2eG8rBaio92Vj9ZU3nvBrTWk8XBbrLyYnlHIBMtNcFZXv2pskr\njJeuztDjapYQ9lLwqaMVF5mWAIVWYvKsZEZDi6IkCsBj9CsV20jrO77rs/pmoOvv\nTyuQIqMJnh9Ro+4QsxNAQEuykWioPvnGFOZ7S/D8rVKTSDQ/Hl2kD9W86rbJ2h0K\naB9kyGcDupi3r08SZhVgUgJVp60cjiOLZ3zomfifUAoe0+FjdzQycpUuIt6v9R6x\n66DNyDwnq1I0yXSMqy2taRQeVgBig4rzwGHYu8UA0ZPnKvJgDpuYAPbwdcGqDm4x\nrNYwsQD7AgMBAAECggEAEAYgrDY1Su4bM634AeOMT/mgfCmCKpsRcj8PyDWoVZQn\nxvCq5/iCIRysaocxFRHydiHPwha7EUF2xNyfYWGYM8OGKVUhx9C/b4yYdBuL5PJa\nIqZNc+JQGRg4AqdRbMbAsAQofytFBHXPsdm+9vAcsrz0yZg0mvUjqAAGHvBHieYv\neaVkUDf9uwnPTZWhgitKvx8qnC6o2liJ9m1AwctwILHHd2uJwHM2ja+29BdkW1Mc\nXQ7UKvt3cNvRYZk4R//q2h2JRnj2fD7KFw427CZ3XJFALTcsERtHrsLq/TcBAhxc\nqnjNmTODgcFDGTPIKDJkcDmBcK/mLdv1DT39xfjiIQKBgQDs+MBKbJKJQLdGKM90\nVRtBGrHhkLjANh+TpUFF6J1tgeMJU6S7/nOGDYueDOk061tJi6LrBRm0jpFejVgv\nrKDovL8mmZQ3pBVPHI219ugOKvEPaXMv50hGG+FSCi2LeeVCmiPY+6u5uWhlR0eO\n1PD72WWPBXAqYxhYsuQRo6ZxYQKBgQDRzHYC1L1ugK/9iC6Dd+GAvUHwsz+GYIil\nzT7wEJMhRJYaSCIYXnD2P7UO3YXBtBmP7NJ0NuMSUU57l2t2MgK7X7dLpEl9BVP+\nvjth9RW/BEwDqnES5oMVTG4uiD13PyQs5lYEEeoQ8n2qEdJULpuX/6g6S5UvO08L\nDAaqj+Xj2wKBgQCHtb2J4bvZ1rIe/+gu2z6Mflk4sMTdbyGOUKmc+6aMvEHlaX2C\nHCrUrpUyono70D2rVMish3M9MBiYaaIDS32rS8y8sl9rrhB2CgguGYSz+GutQ9qv\nqGh/nh/f9+w1YE2nYtb8wMSLzF65AaU9az3HPa0nxxmBbNbYlTp/z5TWgQKBgAor\nmDjw6TH3KRDoTiDvac4c3pp1hgnUmis3dOqcUxWUxpYFkymNQnqdggBzeq12NpsE\nhvs9Qeq8aEqTf5EeQYWl0TPohet6m6TIwgYwpFo7MhgnZg3dgNNCfkDhN+GU/QL0\nM41E9QzXRII/FOwhykcnDJeu3E+K130YmyYfjaxrAoGAHHL4Lmdi4f75M9TjWQ1K\nlX0Vxe49xeDuHWrLDhvmhdrbW3iR5qHENIwK3Iwo/Iey8iBe4f9+MIAqHw8t1/FO\n31hT1WrM12xkX3GIdV56z63Q2rKRmXXOKbxrcv3hWqDAZseTnt7lnNfwuTapMoOJ\ncu2QEetg7iVeT2UC7kn105A=\n-----END PRIVATE KEY-----\n",
+  "client_email": "interact-ocr-nlp@sacred-archway-338609.iam.gserviceaccount.com",
+  "client_id": "105204341273103796182",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/interact-ocr-nlp%40sacred-archway-338609.iam.gserviceaccount.com"
+}
+
+openAI_api_key_string = "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"
